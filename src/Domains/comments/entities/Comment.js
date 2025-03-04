cfc922b7d0a6c68af079da296c1fec9e4fece28d
AbiyaMakruf <aabbiiyyaa@gmail.com>
2025-03-05 05:02:33 +0700

    menambahkan testing untuk konversi date

--- src/Domains/comments/entities/Comment.js
+++ src/Domains/comments/entities/Comment.js
@@ -1,34 +1,33 @@
 class Comment {
-    constructor(payload) {
-      this._verifyPayload(payload);
-      const { id, username, date, content, is_deleted } = payload;
-  
-      this.id = id;
-      this.username = username;
-      this.date = date;
-      this.content = is_deleted ? '**komentar telah dihapus**' : content;
+  constructor(payload) {
+    this._verifyPayload(payload);
+    let { id, username, date, content, is_deleted } = payload;
+
+    if (date instanceof Date) {
+      date = date.toISOString();
+    }
+
+    this.id = id;
+    this.username = username;
+    this.date = date;
+    this.content = is_deleted ? '**komentar telah dihapus**' : content;
+  }
+
+  _verifyPayload({ id, username, date, content, is_deleted }) {          
+    if (!id || !username || !date || content === undefined || is_deleted === undefined) {
+      throw new Error('COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
     }
   
-    _verifyPayload({ id, username, date, content, is_deleted }) {          
-      if (!id || !username || !date || content === undefined || is_deleted === undefined) {
-        throw new Error('COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
-      }
-
-      if (typeof date === 'object' && date instanceof Date) {
-        date = date.toISOString();
-      }      
-  
-      if (
-        typeof id !== 'string' ||
-        typeof username !== 'string' ||
-        typeof date !== 'string' ||
-        typeof content !== 'string' ||
-        typeof is_deleted !== 'boolean'
-      ) {
-        throw new Error('COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION');
-      }
+    if (
+      typeof id !== 'string' ||
+      typeof username !== 'string' ||
+      !(typeof date === 'string' || date instanceof Date) ||
+      typeof content !== 'string' ||
+      typeof is_deleted !== 'boolean'
+    ) {
+      throw new Error('COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION');
     }
   }
-  
-  module.exports = Comment;
-  
+}
+
+module.exports = Comment;
